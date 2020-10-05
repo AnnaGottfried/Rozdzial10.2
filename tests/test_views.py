80c5e25881ac7128f3cc8b32b59a301f2399ecab
Anna <gottfried.anusia@gmail.com>
2020-10-05 12:54:51 +0200

    zastosowanie parametrize

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,8 +3,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import app
 from unittest.mock import Mock
+import pytest
 
-
+'''
 def test_homepage(monkeypatch):
    api_mock = Mock(return_value={'results': []})
    monkeypatch.setattr("tmdb_client.get_movies_list", api_mock)
@@ -13,8 +14,9 @@ def test_homepage(monkeypatch):
        response = client.get('/')
        assert response.status_code == 200
        api_mock.assert_called_once_with('popular')
+'''
 
-'''@pytest.mark.parametrize('n, result', (
+@pytest.mark.parametrize('n, result', (
     ('popular', 200),
     ('now_playing', 200),
     ('top_rated', 200),
@@ -22,9 +24,8 @@ def test_homepage(monkeypatch):
 ))
 def test_homepage(monkeypatch, n, result):
     api_mock = Mock(return_value={'results': [0,1,2,3,4,5,6,7,8,9]})
-    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+    monkeypatch.setattr("tmdb_client.get_movies_list", api_mock)
 
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == result
-        '''
